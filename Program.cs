f24f56fefe6bdfecc264c9da35dca52e78a24daa
Rafael Suarez <rafaelsuarez@Rafaels-MacBook-Pro.local>
2020-11-13 15:26:43 -0500

    First commit

--- Program.cs
+++ Program.cs
@@ -9,13 +9,14 @@ namespace Exercises
             Console.WriteLine("Hello World!");
 
                 Console.WriteLine(SumTriple(2, 2));
-                Console.WriteLine(SumTriple(12, 35));
+                Console.WriteLine(SumTriple(12, 20));
                 Console.WriteLine(SumTriple(-5, 2));
         }
             public static int SumTriple(int a, int b)
             {
                 return a == b ? (a + b) * 3 : a + b;
             }
+
     }
 
 }
