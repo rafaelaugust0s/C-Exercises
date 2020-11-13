c955ce768148db36572ac789626b678df67e6c29
rafaelaugustos <rafaelaugustosc@gmail.com>
2020-11-13 17:46:52 -0500

    exercise 2

--- Program.cs
+++ Program.cs
@@ -6,20 +6,37 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Enter a number - ");
+            int a = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine(SumTriple(2, 2));
-                Console.WriteLine(SumTriple(12, 20));
-                Console.WriteLine(SumTriple(-5, 2));
-        }
-            public static int SumTriple(int a, int b)
+            Console.WriteLine(" Enter a number - ");
+            int b = Convert.ToInt32(Console.ReadLine());
+        
+
+            if (a == 30 || b == 30 || (a + b) == 30)
             {
-                return a == b ? (a + b) * 3 : a + b;
+                Console.WriteLine("true");
+            }else{
+                Console.WriteLine("false");
             }
+
+
+
+            //    Console.WriteLine("Hello World!");
+
+            //        Console.WriteLine(SumTriple(2, 2));
+            //        Console.WriteLine(SumTriple(12, 20));
+            //        Console.WriteLine(SumTriple(-5, 2));
+            //}
+            //    public static int SumTriple(int a, int b)
+            //    {
+            //        return a == b ? (a + b) * 3 : a + b;
+            //    }
+
+            return ;
+        }
 
     }
 
 }
-
-
 
